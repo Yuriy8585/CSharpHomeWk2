a3dbc6e227e8eff2fe29f2e94b212fe69b5e0da1
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-02 20:35:40 +0300

    mistakes

---
+++ Task15/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход цифру, которая приниамет на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.Write("Enter Number of the week form 1 to 7 = ");
+int a = int.Parse(Console.ReadLine());
+if (a == 1)
+{
+    Console.Write("Monday No");
+}
+
+if (a == 2)
+{
+    Console.Write("Tuesday No");
+}
+
+if (a == 3)
+{
+    Console.Write("Wednesday No");
+}
+
+if (a == 4)
+{
+    Console.Write("Thursday No");
+}
+
+if (a == 5)
+{
+    Console.Write("Friday No");
+}
+
+if (a == 6)
+{
+    Console.Write("Saturday Yes");
+}
+
+if (a == 7)
+{
+    Console.Write("Sunday Yes");
+}
+
