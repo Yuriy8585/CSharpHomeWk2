65286e55192f2555d22b32fa278d7e7eaedb464f
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-02 19:57:39 +0300

    End Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число а на выходе показывает последнюю цифру этого числа
-
-Console.Wright("Enter Number = ");
+//
+Console.Write("Enter Number = ");
 int N = int.Parse(Console.ReadLine());
 
 int a = 0;
-int b = 0;
+double b = 0;
 
 a = N%10;
 b = N/10;
 
-Console.Wright("Last Number = ");
-Console.Wrightline(a);
-Console.Wright("Previous Number = " + " " + b);
+Console.Write("Last Number = ");
+Console.WriteLine(a);
+Console.Write("Previous Number = " + " " + b);
