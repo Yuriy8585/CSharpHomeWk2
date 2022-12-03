81d0f4a5587d34e78e6b41e488ebc9cb6a891593
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-03 21:52:42 +0300

    task10

---
+++ Task 13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа и сообщает что третьей цифры нет
+
+Console.Write("Enter Number = ");
+int N = int.Parse(Console.ReadLine());
+
+int a = 0;
+//int b = 0;
+
+a = N%10;
+//b = N/10;
+
+Console.Write("Last Number = ");
+Console.WriteLine(a);
+//Console.Write("Previous Number = " + " " + b);
