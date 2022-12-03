90794fae3b72b2beda4dae92c926c918c388610f
Yuriy Krasnov <118444989+Yuriy8585@users.noreply.github.com>
2022-12-03 21:55:33 +0300

    task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,14 +1,25 @@
 ﻿// Напишите программу, которая принимает на вход трехзначное число а на выходе показывает последнюю цифру этого числа
 //
 Console.Write("Enter Number = ");
-int N = int.Parse(Console.ReadLine());
+int N = Math.Abs(int.Parse(Console.ReadLine())); //Определяем модуль числа
+int NTemp = N;
+int count = 0; // запускаем счетчик разрядов
 
-int a = 0;
-//int b = 0;
-
-a = N%10;
-//b = N/10;
-
-Console.Write("Last Number = ");
-Console.WriteLine(a);
-//Console.Write("Previous Number = " + " " + b);
+while (NTemp > 0)
+{
+    NTemp /= 10;
+    count++;
+}
+if (count - 3 >= 0)
+{
+    while (count - 3 > 0) // число из последних трех разрядов
+    {
+        N = N / 10;
+        count--;
+    }
+    Console.WriteLine($"Тhird number {N % 10}"); // определяем число
+}
+else
+{
+    Console.WriteLine("No third number");
+}
